0e8deade30e2a6025193797361246a7fa7cd526c
unknown <animesh.sakorikar@gmail.com>
2023-12-27 19:17:10 -0800

    feat(flutter-tools): add color overlay for flutter projects

--- lua/anisako/flutter-tools/config.lua
+++ lua/anisako/flutter-tools/config.lua
@@ -3,6 +3,9 @@ local lspconfigSettings = require("anisako.lsp.lspconfig")
 
 require("flutter-tools").setup {
   lsp = {
+    color = {
+      enabled = true,
+    },
     on_attach = lspconfigSettings.onattach,
     capabilities = lspconfigSettings.capabilities,
   },
