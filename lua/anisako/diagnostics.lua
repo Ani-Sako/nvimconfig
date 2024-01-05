bc0d98bf744b9d1f4aebfeddb6a91b73efb1d648
unknown <animesh.sakorikar@gmail.com>
2024-01-04 22:54:59 -0800

    feat(diagnostics): display diagnositics in floating window
    Place cursor on error and press '<leader>e' to display floating window
    error message

---
+++ lua/anisako/diagnostics.lua
@@ -0,0 +1,17 @@
+vim.diagnostic.config ({
+  virtual_text = false,
+})
+
+-- Remap to show line diagnostics
+vim.keymap.set('n', '<leader>e', function() vim.diagnostic.open_float(nil, {focus=false}) end)
+
+-- Todo: remaps for next and previous diagnostic error (vim diagnostic.goto_next)
+
+-- -- Show line diagnostics automatically in hover window
+-- vim.o.updatetime = 250
+-- vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+--   group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+--   callback = function ()
+--     vim.diagnostic.open_float(nil, {focus=false})
+--   end
+-- })
