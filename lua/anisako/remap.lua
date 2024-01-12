3f915e2efafe54ef34b255e394f6267c672f77ff
unknown <animesh.sakorikar@gmail.com>
2024-01-11 22:05:04 -0800

    fix(remap): accidental erase of remap to repeatedly paste the same yanked text

--- lua/anisako/remap.lua
+++ lua/anisako/remap.lua
@@ -21,7 +21,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("x", "<leader>p", "\"_dP")
 
 -- Change current window width by moving rhs border
 -- incrase current window width
