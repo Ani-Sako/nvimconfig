fdf8bc083ccda3f7da56e5ecf6723a05c4bfc9c3
unknown <animesh.sakorikar@gmail.com>
2024-02-07 21:28:42 -0800

    feat: change theme

--- lua/anisako/plugins/onedark.lua
+++ lua/anisako/plugins/onedark.lua
@@ -1,12 +1,20 @@
-return
-  {
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-      require('onedark').setup {
-        style = 'darker',
-      }
-      require('onedark').load()
-    end,
-  }
+-- return
+--   {
+--     'navarasu/onedark.nvim',
+--     priority = 1000,
+--     config = function()
+--       vim.cmd.colorscheme 'onedark'
+--       require('onedark').setup {
+--         style = 'darker',
+--       }
+--       require('onedark').load()
+--     end,
+--   }
+
+return 
+  { 
+    "EdenEast/nightfox.nvim", 
+    config = function ()
+      vim.cmd("colorscheme carbonfox")
+    end
+  } 
