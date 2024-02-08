4f23d899521fde8e9a1d27ab84a580d3d33ce9d0
unknown <animesh.sakorikar@gmail.com>
2024-02-07 19:35:31 -0800

    feat: modify lualine theme

--- lua/anisako/plugins/lualine.lua
+++ lua/anisako/plugins/lualine.lua
@@ -3,26 +3,13 @@ return
     -- set lualine as statusline
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons'},
-    -- see `:help lualine.txt`
-    -- opts = {
-    --   options = {
-    --     icons_enabled = false,
-    --     theme = 'molokai',
-    --     component_separators = '|',
-    --     section_separators = '',
-    --   },
-    -- },
     config = function ()
-      -- Change last item on statusbar from lineno,columnno to window display
       require('lualine').setup({
-      options = {
-        icons_enabled = false,
-        theme = 'codedark',
-        component_separators = '|',
-        section_separators = '',
-      },
-        sections = {
-          lualine_z = {'buffers'}
+        options = {
+          icons_enabled = false,
+          theme = 'iceberg_dark',
+          component_separators = '|',
+          section_separators = '',
         },
       })
     end,
