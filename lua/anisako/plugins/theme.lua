13568012d440876d1c982065d071fe6d3ecb576c
Ani-Sako <animesh.sakorikar@gmail.com>
2024-03-17 22:37:49 -0700

    feat: update nvimcmp bindings, remove unused bindings

--- lua/anisako/plugins/theme.lua
+++ lua/anisako/plugins/theme.lua
@@ -33,11 +33,18 @@
 --     vim.cmd("colorscheme rose-pine-main")
 --   end
 -- }
---
 
 return {
-  'tomasiser/vim-code-dark', 
-  config = function ()
-    vim.cmd("colorscheme codedark")
-  end
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  init = function()
+    -- Load the colorscheme here.
+    -- Like many other themes, this one has different styles, and you could load
+    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    vim.cmd.colorscheme 'tokyonight-night'
+
+    -- You can configure highlights by doing something like
+    vim.cmd.hi 'Comment gui=none'
+  end,
 }
