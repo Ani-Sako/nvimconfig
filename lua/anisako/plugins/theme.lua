8dce024acd66bcf307cdca52088c6b9e3ad00115
Ani-Sako <animesh.sakorikar@gmail.com>
2024-03-17 20:58:20 -0700

    feat: use vscode color scheme (skullemoji)

--- lua/anisako/plugins/theme.lua
+++ lua/anisako/plugins/theme.lua
@@ -19,17 +19,25 @@
 --   }
 --
 
+-- return {
+--   "rose-pine/neovim",
+--   name = "rose-pine",
+--   config = function ()
+--     require("rose-pine").setup({
+--       variant = "main",
+--       styles = {
+--         italic = false,
+--         -- transparency = true,
+--       },
+--     })
+--     vim.cmd("colorscheme rose-pine-main")
+--   end
+-- }
+--
+
 return {
-  "rose-pine/neovim",
-  name = "rose-pine",
+  'tomasiser/vim-code-dark', 
   config = function ()
-    require("rose-pine").setup({
-      variant = "main",
-      styles = {
-        italic = false,
-        -- transparency = true,
-      },
-    })
-    vim.cmd("colorscheme rose-pine-main")
+    vim.cmd("colorscheme codedark")
   end
 }
--- lua/anisako/settings.lua
+++ lua/anisako/settings.lua
@@ -25,7 +25,7 @@ vim.o.timeoutlen = 300
 vim.o.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
--- vim.o.termguicolors = true
+vim.o.termguicolors = true
 
 vim.o.wrap = false
 
